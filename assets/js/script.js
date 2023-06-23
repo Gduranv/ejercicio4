b9dcaaa29cd0c2e2b7e5c70687c18ea7661079e3
gduranv <victorduran1003@gmail.com>
2023-06-23 11:22:46 -0400

    Changes to be committed: new file:   assets/css/materialize.css new file:   assets/css/materialize.min.css new file:   assets/css/styles.css new file:   assets/img/urbe1.jpg new file:   assets/img/urbe2.png new file:   assets/img/urbe22.jpg new file:   assets/img/urbe3.webp new file:   assets/js/materialize.js new file:   assets/js/materialize.min.js new file:   assets/js/script.js new file:   index.html

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded",() => {
+
+    //Navegacion Menu
+  
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  
+    //Slider
+  
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, {
+      indicators: false,
+      height: 500,
+      duration: 500,
+      interval:3000
+    });
+  
+  });
